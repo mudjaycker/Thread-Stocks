d8b70e522ae07883114357411c8ce30bea864272
mudjaycker <tombutoyi@gmail.com>
2021-07-03 09:48:08 +0200

    we added Scanner method

--- src/App.java
+++ src/App.java
@@ -1,11 +1,21 @@
+import java.util.Scanner;
 
-    public class App {
+public class App {
     public static void main(String[] args) throws Exception {
-        Stocks st = new Stocks();
-        ThreadREmQty THR = new ThreadREmQty(st, 50, 12);
-        ThreadAddQty THA = new ThreadAddQty(st, 30, 500);
+        try ( Scanner scanner = new Scanner( System.in ) ) {
+            
+            System.out.print( "Type the quantity to add: " );
+            int addedQty = scanner.nextInt();
+            
+            System.out.print( "Type the quantity to remove : " );
+            int removedQtity = scanner.nextInt();
 
-        THA.start();
-        THR.start();
+            Stocks st = new Stocks();
+            ThreadREmQty THR = new ThreadREmQty(st, 50, removedQtity);
+            ThreadAddQty THA = new ThreadAddQty(st, 30, addedQty);
+
+            THA.start();
+            THR.start();
+        }
     }
 }
